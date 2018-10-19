cdbb0b19cae6712bafab6b0ba1b68a5bddfff918
Nandu kmg <nandukrishnan55555@gmail.com>
2018-10-19 20:48:34 +0530

    add Rehab xml

---
+++ app/src/main/java/com/example/spot_that_fire/RehabActivity.java
@@ -0,0 +1,13 @@
+package com.example.spot_that_fire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RehabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rehab);
+    }
+}
