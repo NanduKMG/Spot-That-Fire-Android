86b3c3835fb64194478f7f03f5d637ccb43c6d44
Nandu kmg <nandukrishnan55555@gmail.com>
2018-10-20 06:22:27 +0530

    add everythign

---
+++ app/src/main/java/com/example/spot_that_fire/SigninActivity.java
@@ -0,0 +1,14 @@
+package com.example.spot_that_fire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+
+}
