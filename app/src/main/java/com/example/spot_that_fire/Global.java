9ba6b983685346065639d37cc5ca29221ed2eb33
amrithm98 <amrithchallenger@gmail.com>
2018-10-26 17:44:50 +0530

    Refactor Reporting Page

--- app/src/main/java/com/example/spot_that_fire/Global.java
+++ app/src/main/java/com/example/spot_that_fire/Global.java
@@ -2,6 +2,7 @@ package com.example.spot_that_fire;
 
 public class Global
 {
-    public static String BASE_URL = "http://192.168.20.88:3030/";
+//    public static String BASE_URL = "http://192.168.20.88:3030/";
+    public static String BASE_URL = "http://discreet-game.glitch.me/";
     public static String settings = "";
 }
